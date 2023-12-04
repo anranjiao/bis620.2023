f66fb2674bd511a29897778de4816b6e8c10b7a9
Min Zhu <84722601+MinZhu123@users.noreply.github.com>
2023-12-04 18:32:03 -0500

    Delete R/launchmyapp.R

--- R/launchmyapp.R
+++
@@ -1,7 +0,0 @@
-launchmyapp <- function() {
-  appdir <- system.file("shinyapp", package = "bis620.2023")
-  if (appdir == "") {
-    stop("Shiny app not found in the package")
-  }
-  shiny::runApp(appdir)
-}
